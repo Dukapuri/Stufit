10034eaff7dc826267fe3ea078d2e1efdc88908f
황성태 <vivian991031@naver.com>
2023-05-24 21:31:56 +0900

    충돌 해결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,14 @@
-<<<<<<< Updated upstream
-=======
 import './App.css';
 import Header from './component/header';
 import Section from './component/section';
->>>>>>> Stashed changes
 
 
 function App() {
   return (
       <div className="App">
-<<<<<<< Updated upstream
 
-=======
         <Header></Header>
         <Section></Section>
->>>>>>> Stashed changes
       </div>
   );
 };
